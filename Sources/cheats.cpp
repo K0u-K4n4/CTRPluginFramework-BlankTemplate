a5427b6a186e3dde41083e599ddd09161c35cc9d
K0u-K4n4 <107753658+K0u-K4n4@users.noreply.github.com>
2022-06-18 21:27:36 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -7,147 +7,138 @@ float dataf = 0;
 
 namespace CTRPluginFramework
 {
-void KeyStrokesButtom(MenuEntry* entry){
-  const Screen &top_screen = OSD::GetTopScreen();
-  u32 x = 5;
-  u32 y = 200;
-  Color foreground = Color::SkyBlue;
-  Color background = Color::Black;
-  std::string keys;
-  if(Controller::IsKeysDown(A)) {
-    keys += "A  ";
-    top_screen.Draw(keys, x, y, foreground, background);
+  void KeyStrokesButtom(MenuEntry* entry) {
+    const Screen &KSTSC = OSD::GetTopScreen();
+    std::string IsK = "";
+    u32 KSTX = 10;
+    u32 KSTY = 50;
+    Color foreground = Color::White;
+    Color background = Color::Black;
+    if(Controller::IsKeysDown(A)) {
+      IsK += "A  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(B)) {
+      IsK += "B  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(X)) {
+      IsK += "X  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(Y)) {
+      IsK += "Y  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(R)) {
+      IsK += "R  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(L)) {
+      IsK += "L  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(ZR)) {
+      IsK += "ZR  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(ZL)) {
+      IsK += "ZL  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(Start)) {
+      IsK += "Start  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
+    if(Controller::IsKeysDown(Select)) {
+      IsK += "Select  ";
+      KSTSC.DrawSysfont(IsK, KSTX, KSTY, foreground, background);
+    }
   }
-  if(Controller::IsKeysDown(B)) {
-    keys += "B  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(X)) {
-    keys += "X  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(Y)) {
-    keys += "Y  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(R)) {
-    keys += "R  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(L)) {
-    keys += "L  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(ZR)) {
-    keys += "ZR  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(ZL)) {
-    keys += "ZL  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(Start)) {
-    keys += "Start  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-  if(Controller::IsKeysDown(Select)) {
-    keys += "Select  ";
-    top_screen.Draw(keys, x, y, foreground, background);
-  }
-}
-void KeyStrokesPad(MenuEntry* entry) {
-  const Screen &topkeyp = OSD::GetTopScreen();
-  const Screen &topkeyp2 = OSD::GetTopScreen();
-  u32 x2 = 350;
-  u32 y2 = 5;
-  Color foreground = Color::SkyBlue;
-  Color background = Color::Black;
-  std::string pad;
-  std::string pad2;
-  pad = "_";
-  pad2 = "_ _ _";
-  u8 Disdown;
-  if(Controller::IsKeysDown(CPadUp)) {
-    pad = "X";
+  void KeyStrokesPad(MenuEntry* entry) {
+    if(Controller::IsKeysDown(CPadUp) || Controller::IsKeysDown(CPadDown) || Controller::IsKeysDown(CPadRight) || Controller::IsKeysDown(CPadLeft)) {
+    const Screen &Pad = OSD::GetTopScreen();
+    const Screen &Pat = OSD::GetTopScreen();
+    u32 KSTX = 360;
+    u32 KSTY = 5;
+    Color foreground = Color::White;
+    Color background = Color::Black;
+    std::string Pad1 = "_";
+    std::string Pad2 = "_ _ _";
+    if(Controller::IsKeysDown(CPadUp)) {
+      Pad1 = "X";
+      if(Controller::IsKeysDown(CPadRight)) {
+        Pad2 = "_ _ X";
+      }
+      if(Controller::IsKeysDown(CPadLeft)) {
+        Pad2 = "X _ _";
+      }
+    }
+    if(Controller::IsKeysDown(CPadDown)) {
+      if(Controller::IsKeysDown(CPadRight)) {
+        Pad2 = "_ X X";
+      }else{
+      if(Controller::IsKeysDown(CPadLeft)) {
+        Pad2 = "X X _";
+      }else{
+        Pad2 = "_ X _";
+      }
+      }
+    }else{
     if(Controller::IsKeysDown(CPadRight)) {
-      pad2 = "_ _ X"; 
-    }else{
+        Pad2 = "_ _ X";
+      }
     if(Controller::IsKeysDown(CPadLeft)) {
-      pad2 = "X _ _";
+        Pad2 = "X _ _";
+      }
     }
-    }
-  }else{
-  if(Controller::IsKeysDown(CPadDown)) {
-    if(Controller::IsKeysDown(CPadRight)) {
-      pad2 = "_ X X"; 
-    }else{
-    if(Controller::IsKeysDown(CPadLeft)) {
-      pad2 = "X X _";
-    }else{
-    pad2 = "_ X _";
-    }
-    }
-  }else{
-    if(Controller::IsKeysDown(CPadLeft)) {
-      pad2 = "X _ _";
-    }else{
-     if(Controller::IsKeysDown(CPadRight)) {
-       pad2 = "_ _ X";
-     }
-    }
-    }
-  
+  Pad.DrawSysfont(Pad1, KSTX+12, KSTY, foreground, background);
+  Pat.DrawSysfont(Pad2, KSTX, KSTY+13, foreground, background);
   }
-  topkeyp.Draw(pad, x2+12, y2, foreground, background);
-  topkeyp2.Draw(pad2, x2, y2+13, foreground, background);
-}
-void KeyStrokesDPad(MenuEntry* entry) {
-  const Screen &topkeyp = OSD::GetTopScreen();
-  const Screen &topkeyp2 = OSD::GetTopScreen();
-  u32 x2 = 5;
-  u32 y2 = 175;
-  Color foreground = Color::SkyBlue;
-  Color background = Color::Black;
-  std::string pad;
-  std::string pad2;
-  pad = "_";
-  pad2 = "_ _ _";
-  u8 Disdown;
-  if(Controller::IsKeysDown(DPadUp)) {
-    pad = "X";
-    if(Controller::IsKeysDown(DPadRight)) {
-      pad2 = "_ _ X"; 
-    }else{
-    if(Controller::IsKeysDown(DPadLeft)) {
-      pad2 = "X _ _";
-    }
-    }
-  }else{
-  if(Controller::IsKeysDown(DPadDown)) {
-    if(Controller::IsKeysDown(DPadRight)) {
-      pad2 = "_ X X"; 
-    }else{
-    if(Controller::IsKeysDown(DPadLeft)) {
-      pad2 = "X X _";
-    }else{
-    pad2 = "_ X _";
-    }
-    }
-  }else{
-    if(Controller::IsKeysDown(DPadLeft)) {
-      pad2 = "X _ _";
-    }else{
-     if(Controller::IsKeysDown(DPadRight)) {
-       pad2 = "_ _ X";
-     }
-    }
-    }
-  
   }
-  topkeyp.Draw(pad, x2+12, y2, foreground, background);
-  topkeyp2.Draw(pad2, x2, y2+13, foreground, background);
-}
-void Readanim(MenuEntry* entry) {
+  void KeyStrokesDPad(MenuEntry* entry) {
+    if(Controller::IsKeysDown(DPadUp) || Controller::IsKeysDown(DPadDown) || Controller::IsKeysDown(DPadRight) || Controller::IsKeysDown(DPadLeft)) {
+    const Screen &Pad = OSD::GetTopScreen();
+    const Screen &Pat = OSD::GetTopScreen();
+    u32 KSTX = 5;
+    u32 KSTY = 210;
+    Color foreground = Color::White;
+    Color background = Color::Black;
+    std::string Pad1 = "_";
+    std::string Pad2 = "_ _ _";
+    if(Controller::IsKeysDown(DPadUp)) {
+      Pad1 = "X";
+      if(Controller::IsKeysDown(DPadRight)) {
+        Pad2 = "_ _ X";
+      }
+      if(Controller::IsKeysDown(DPadLeft)) {
+        Pad2 = "X _ _";
+      }
+    }
+    if(Controller::IsKeysDown(DPadDown)) {
+      if(Controller::IsKeysDown(DPadRight)) {
+        Pad2 = "_ X X";
+      }else{
+      if(Controller::IsKeysDown(DPadLeft)) {
+        Pad2 = "X X _";
+      }else{
+        Pad2 = "_ X _";
+      }
+      }
+    }else{
+    if(Controller::IsKeysDown(DPadRight)) {
+        Pad2 = "_ _ X";
+      }
+    if(Controller::IsKeysDown(DPadLeft)) {
+        Pad2 = "X _ _";
+      }
+    }
+  Pad.DrawSysfont(Pad1, KSTX+12, KSTY, foreground, background);
+  Pat.DrawSysfont(Pad2, KSTX, KSTY+13, foreground, background);
+  }
+  }
+  void Readanim(MenuEntry* entry) {
+    if(Controller::IsKeysDown(R + L + DPadRight)) {
   u8 HP;
   u8 Sutam;
   u16 sharp;
@@ -165,42 +156,49 @@ void Readanim(MenuEntry* entry) {
   Process::Read8(offset + 0x0F5E , Sutam);
   Process::Read16(offset + 0x2214 , sharp);
   Process::Read8(offset + 0x0CF4 , field);
-  HPi.Draw("HP : " + Utils::Format("%d" , HP), x4, y4, foreground, background);
-  Sutami.Draw("Moving : " + Utils::Format("%d" , Sutam), x4, y4+10, foreground, background);
-  sharpi.Draw("Sharpness : " + Utils::Format("%d" , sharp), x4, y4+20, foreground, background);
-  fieldi.Draw("Field : " + Utils::Format("%d" , field), x4, y4+30, foreground, background);
+  HPi.DrawSysfont("HP : " + Utils::Format("%d" , HP), x4, y4, foreground, background);
+  Sutami.DrawSysfont("スタミナ : " + Utils::Format("%d" , Sutam), x4, y4+10, foreground, background);
+  sharpi.DrawSysfont("切れ味 : " + Utils::Format("%d" , sharp), x4, y4+20, foreground, background);
+  fieldi.DrawSysfont("現在のフィールド : " + Utils::Format("%d" , field), x4, y4+30, foreground, background);
+    }
 }
-void Readanim2(MenuEntry* entry) {
-  const Screen &HRi = OSD::GetTopScreen();
-  const Screen &atki = OSD::GetTopScreen();
-  const Screen &dfci = OSD::GetTopScreen();
-  const Screen &abli = OSD::GetTopScreen();
-  const Screen &monyi = OSD::GetTopScreen();
-  const Screen &pointi = OSD::GetTopScreen();
-  u32 x5 = 10;
-  u32 y5 = 50;
-  Color foreground = Color::SkyBlue;
-  Color background = Color::Black;
-  u16 atk;
-  u16 dfc;
-  u16 abl;
-  u16 HR;
-  u32 Money;
-  u32 point;
-  Process::Read16(0x831B450 , atk);
-  Process::Read16(0x831B45E , dfc);
-  Process::Read16(0x831B45A , abl);
-  Process::Read16(0x831B76A , HR);
-  Process::Read32(0x83B3818 , Money);
-  Process::Read32(0x83B3820 , point);
-  dfc += 1;
-  atki.Draw("attacks : " + Utils::Format("%d" , atk), x5, y5, foreground, background);
-  dfci.Draw("Defenses : " + Utils::Format("%d" , dfc), x5, y5+10, foreground, background);
-  abli.Draw("attributes : " + Utils::Format("%d" , abl), x5, y5+20, foreground, background);
-  HRi.Draw("HR : " + Utils::Format("%d" , HR), x5, y5+30, foreground, background);
-  monyi.Draw("Money : " + Utils::Format("%d" , Money), x5, y5+40, foreground, background);
-  pointi.Draw("RRI Point : " + Utils::Format("%d" , point), x5, y5+50, foreground, background);
+  void Readanim2(MenuEntry* entry) {
+    if(Controller::IsKeysDown(R + L + DPadLeft)) {
+      const Screen &HRi = OSD::GetTopScreen();
+      const Screen &atki = OSD::GetTopScreen();
+      const Screen &dfci = OSD::GetTopScreen();
+      const Screen &abli = OSD::GetTopScreen();
+      const Screen &monyi = OSD::GetTopScreen();
+      const Screen &pointi = OSD::GetTopScreen();
+      u32 x5 = 10;
+      u32 y5 = 50;
+      Color foreground = Color::SkyBlue;
+      Color background = Color::Black;
+      u16 atk;
+      u16 dfc;
+      u16 abl;
+      u16 HR;
+      u32 Money;
+      u32 point;
+      Process::Read16(0x831B450 , atk);
+      Process::Read16(0x831B45E , dfc);
+      Process::Read16(0x831B45A , abl);
+      Process::Read16(0x831B76A , HR);
+      Process::Read32(0x83B3818 , Money);
+      Process::Read32(0x83B3820 , point);
+      dfc += 1;
+      atki.DrawSysfont("攻撃力 : " + Utils::Format("%d" , atk), x5, y5, foreground, background);
+      dfci.DrawSysfont("防御力 : " + Utils::Format("%d" , dfc), x5, y5+10, foreground, background);
+      abli.DrawSysfont("攻撃属性値 : " + Utils::Format("%d" , abl), x5, y5+20, foreground, background);
+      HRi.DrawSysfont("HR : " + Utils::Format("%d" , HR), x5, y5+30, foreground, background);
+      monyi.DrawSysfont("所持金 : " + Utils::Format("%d" , Money), x5, y5+40, foreground, background);
+      pointi.DrawSysfont("龍歴院ポイント : " + Utils::Format("%d" , point), x5, y5+50, foreground, background);
+    }
 }
 }
+
+
+
+
 
 
